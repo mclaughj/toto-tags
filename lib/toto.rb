096e0c3d762813176b4a2d27ab53360df9509306
mclaughj <josephmclaughlin@me.com>
2013-04-10 07:55:58 -0600

    Added ability to write link posts

--- lib/toto.rb
+++ lib/toto.rb
@@ -293,6 +293,7 @@ module Toto
 
     def title()   self[:title] || "an article"               end
     def tags()    self[:tags]  || ""                         end
+    def link()    self[:link]  || ""                         end
     def date()    @config[:date].call(self[:date])           end
     def author()  self[:author] || @config[:author]          end
     def to_html() self.load; super(:article, @config)        end
